b8b47b92791e37fa9f40d7aa477f4a8a31e750fc
Edgars <Edgars@Edgars-PC>
2019-01-28 22:50:17 +0200

    Fixed formating

--- test_stm32_adc_dma/src/main.c
+++ test_stm32_adc_dma/src/main.c
@@ -34,7 +34,7 @@ int main(void) {
 
 	(*((int *) (0x40012000u + 0x8u))) |= 1 << 30; //start ADC
 
-	while(1);
+	while (1);
 }
 
 void init_GPIOB(void) {
@@ -59,8 +59,7 @@ void init_GPIOA(void) {
 	//configure GPIOB
 	GPIO_InitTypeDef GPIO_InitDef; //structure
 
-	GPIO_InitDef.GPIO_Pin = (GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3
-			| GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7);
+	GPIO_InitDef.GPIO_Pin = (GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3| GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7);
 	GPIO_InitDef.GPIO_Mode = GPIO_Mode_AN;
 	GPIO_Init(GPIOA, &GPIO_InitDef); //pin 0, 1, 2 analog
 }
@@ -72,8 +71,7 @@ void init_GPIOC(void) {
 	//enable clock
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
-	GPIO_InitDef.GPIO_Pin = (GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3
-			| GPIO_Pin_4 | GPIO_Pin_5);
+	GPIO_InitDef.GPIO_Pin = (GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3| GPIO_Pin_4 | GPIO_Pin_5);
 	GPIO_InitDef.GPIO_Mode = GPIO_Mode_AN;
 	GPIO_Init(GPIOB, &GPIO_InitDef);
 
@@ -110,18 +108,12 @@ void init_ADC1(void) {
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_9, 8, ADC_SampleTime_480Cycles);
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 9, ADC_SampleTime_480Cycles);
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_9, 10, ADC_SampleTime_480Cycles);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 11,
-			ADC_SampleTime_480Cycles);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 12,
-			ADC_SampleTime_480Cycles);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_12, 13,
-			ADC_SampleTime_480Cycles);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_13, 14,
-			ADC_SampleTime_480Cycles);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_14, 15,
-			ADC_SampleTime_480Cycles);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_15, 16,
-			ADC_SampleTime_480Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 11,ADC_SampleTime_480Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 12,ADC_SampleTime_480Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_12, 13,ADC_SampleTime_480Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_13, 14,ADC_SampleTime_480Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_14, 15,ADC_SampleTime_480Cycles);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_15, 16,ADC_SampleTime_480Cycles);
 
 	ADC_Cmd(ADC1, ENABLE);  //enable ADC1
 	ADC_DMACmd(ADC1, ENABLE); //enable DMA for ADC
@@ -133,8 +125,7 @@ void init_DMA2(void) {
 	(*((uint32_t *) (0x40026400u + 0x18u))) = (uint32_t) 0x4001204cu; //set ADC1_DR port address DMA_SxPAR
 	(*((uint32_t *) (0x40026400u + 0x1cu))) = (uint32_t) &adc1[0]; // SRAM adrese..vajag aspkatiities, vai stack kkur tur neparaskta
 	(*((uint32_t *) (0x40026400u + 0x14u))) = 14; //number of data to be transferred
-	(*((uint32_t *) (0x40026400u + 0x10u))) |= (1 << 8 | 1 << 10 | 1 << 11
-			| 1 << 13); //(1<<8|1<<10|1<<12|1<<14);//circular mode,memory pointer is incremented,32 bit peripheral and 32 bit memory
+	(*((uint32_t *) (0x40026400u + 0x10u))) |= (1 << 8 | 1 << 10 | 1 << 11| 1 << 13); //(1<<8|1<<10|1<<12|1<<14);//circular mode,memory pointer is incremented,32 bit peripheral and 32 bit memory
 	(*((uint32_t *) (0x40026400u + 0x10u))) |= 1 << 0; //starts DMA
 // DMA_Cmd(DMA2_Channel1, ENABLE);
 }
